e257828a94d564eb83877ded51bfebb69700bf05
Ysee Monnier <yseemonnier@gmail.com>
2017-10-11 22:52:56 +0200

    add basic Coordinator class

--- PNPC-iOS/Coordinator/Coordinator.swift
+++ PNPC-iOS/Coordinator/Coordinator.swift
@@ -6,4 +6,23 @@
 //  Copyright © 2017 ymonnier. All rights reserved.
 //
 
-import Foundation
+/// The Coordinator protocol
+public protocol Coordinator: class {
+    /// The array containing any child Coordinators
+    var childCoordinators: [Coordinator] { get set }
+    
+    /// Start the coordination.
+    func start()
+}
+
+public extension Coordinator {
+    /// Add a child coordinator to the parent
+    public func addChildCoordinator(_ childCoordinator: Coordinator) {
+        self.childCoordinators.append(childCoordinator)
+    }
+    
+    /// Remove a child coordinator from the parent
+    public func removeChildCoordinator(_ childCoordinator: Coordinator) {
+        self.childCoordinators = self.childCoordinators.filter { $0 !== childCoordinator }
+    }
+}
--- PNPC-iOS/Coordinator/RootViewCoordinator.swift
+++ PNPC-iOS/Coordinator/RootViewCoordinator.swift
@@ -6,4 +6,16 @@
 //  Copyright © 2017 ymonnier. All rights reserved.
 //
 
-import Foundation
+import UIKit
+
+public protocol RootViewControllerProvider: class {
+    /// The coordinators 'rootViewController'.
+    /// It helps to think of this as the view
+    /// controller that can be used to dismiss
+    /// the coordinator from the view hierarchy.
+    var rootViewController: UIViewController { get }
+}
+
+/// A Coordinator type that provides a root UIViewController
+public typealias RootViewCoordinator = Coordinator & RootViewControllerProvider
+
