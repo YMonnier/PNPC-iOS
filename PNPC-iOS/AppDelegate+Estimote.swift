fdad48dbd70e0b4d5ca8c3014a1771857f7b7291
Ysee Monnier <yseemonnier@gmail.com>
2017-10-14 00:03:35 +0200

    add EstimoteSDK Lib

---
+++ PNPC-iOS/AppDelegate+Estimote.swift
@@ -0,0 +1,9 @@
+//
+//  AppDelegate+Estimote.swift
+//  PNPC-iOS
+//
+//  Created by Ysée MONNIER on 13/10/2017.
+//  Copyright © 2017 ymonnier. All rights reserved.
+//
+
+import Foundation
---
+++ PNPC-iOS/ObjCBridge.h
@@ -0,0 +1,13 @@
+//
+//  ObjCBridge.h
+//  PNPC-iOS
+//
+//  Created by Ysée MONNIER on 13/10/2017.
+//  Copyright © 2017 ymonnier. All rights reserved.
+//
+
+#ifndef ObjCBridge_h
+#define ObjCBridge_h
+
+
+#endif /* ObjCBridge_h */
